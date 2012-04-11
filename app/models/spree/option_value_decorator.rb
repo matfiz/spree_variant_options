dbf2464d13489b298ece3b14a9f927989ee365f6
Grzegorz Brzezinka <grzegorz@brzezinka.eu>
2012-04-12 00:47:07 +0200

    dodanie pola definiujacego kolor HEX w opcji wariantu

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,6 +1,7 @@
 Spree::OptionValue.class_eval do
 
   default_scope order("#{quoted_table_name}.position")
+  validates :hex_color, :length => { :is => 6, :allow_blank => true }
 
   has_attached_file :image,
     :styles        => { :small => '40x30#', :large => '140x110#' },
@@ -10,6 +11,14 @@ Spree::OptionValue.class_eval do
 
   def has_image?
     image_file_name && !image_file_name.empty?
+  end
+  
+  def hex_color=(value)
+    write_attribute(:hex_color, value.gsub('#', ''))
+  end
+  
+  def formatted_hex_color
+    "##{read_attribute(:hex_color)}" if hex_color?
   end
 
   scope :for_product, lambda { |product| select("DISTINCT #{table_name}.*").where("spree_option_values_variants.variant_id IN (?)", product.variant_ids).joins(:variants)
