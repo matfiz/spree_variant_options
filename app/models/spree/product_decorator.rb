ae1905fc1f0628aca49cc27ff11cd6de0ff2bca3
Grzegorz Brzezinka <grzegorz@brzezinka.eu>
2012-07-06 08:27:30 +0200

    color first - second try

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,6 +2,9 @@ Spree::Product.class_eval do
 
   def option_values
     @_option_values ||= Spree::OptionValue.for_product(self).order(:position).sort_by {|ov| ov.option_type.position }.sort{|a,b| a.presentation<=>b.presentation}
+    kolor = Spree::OptionValue.for_product(self).where("presentation REGEXP 'kolor'").order(:position).sort_by {|ov| ov.option_type.position }
+    rozmiar = Spree::OptionValue.for_product(self).where("presentation REGEXP '[^(kolor)]'").order(:position).sort_by {|ov| ov.option_type.position }
+    @_option_values = kolor + rozmiar
   end
 
   def grouped_option_values
